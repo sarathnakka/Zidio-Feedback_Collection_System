e98e6d04e7ecd6c38b61facd7e53b3eac44c4e00
Bhavesh Sompura <56756309+Bhavesh-Sompura@users.noreply.github.com>
2024-05-15 23:53:11 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -80,54 +80,30 @@ function Home() {
               <a className="navbar-brand" href="">
                 <img src={logo} alt="" /> &nbsp;
                 <span>Feedback Collection System</span>
-              </a>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="{'navbarSupportedContent'}"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon" />
-              </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              </a>                             
+              <div className="collapse navbar-collapse" id="navbarSupported" style={{marginLeft:'20px'}}>
                 <ul className="navbar-nav  ">
                   <li className="nav-item active">
-                    <a className="nav-link" href="index.html">
+                    <a className="nav-link" href="">
                       Home <span className="sr-only">(current)</span>
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="about.html">
-                      {" "}
+                    <a className="nav-link" href="#about_section layout_padding">                      
                       About
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="work.html">
-                      Work{" "}
+                    <a className="nav-link" href="/login">
+                     Login
                     </a>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="category.html">
-                      {" "}
-                      Category{" "}
+                    <a className="nav-link" href="/register">
+                     Signup
                     </a>
                   </li>
-                </ul>
-                <div className="user_option">
-                  <a href="">
-                    <span>Login</span>
-                  </a>
-                  <form className="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
-                    <button
-                      className="btn  my-2 my-sm-0 nav_search-btn"
-                      type="submit"
-                    />
-                  </form>
-                </div>
+                </ul>               
               </div>
               <div>
                 <div className="custom_menu-btn ">
